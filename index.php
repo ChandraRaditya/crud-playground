3da6d49385c54fc58fa8454ecf5ea56af28691e9
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-10-29 22:36:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+   <div class="home">
+     <div class="home-container">
+        <h1 class="title">Table Data Mahasiswa</h1>
+        <table>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Nim</th>
+                <th>Fakultas</th>
+                <th>Prodi</th>
+            </tr>
+            <tr>
+                <td>No</td>
+                <td>Nama</td>
+                <td>Nim</td>
+                <td>Fakultas</td>
+                <td>Prodi</td>
+            </tr>
+        </table>
+     </div>
+   </div>
+</body>
+</html>
